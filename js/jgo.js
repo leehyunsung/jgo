6bad36cc78aefb78193433125e73f01a8bf6dc61
leehyunsung <0175854365@naver.com>
2014-01-13 08:17:19 +0900

    최초 등록

--- js/jgo.js
+++ js/jgo.js
@@ -1,3 +1,4 @@
+/* 날짜를 주어진 구분자로 가져옴 */
 function getTodayYYYYMMDD( seperator ) {
 	if( !seperator ) {
 		seperator = "-";
@@ -16,3 +17,19 @@ function getTodayYYYYMMDD( seperator ) {
 	today = yyyy+seperator+mm+seperator+dd;
 	return today;
 }
+//url에 추가된 get parameter값을 가져옴. 전체 호출 : getUrlVars, 이름으로 호출 : getUrlVar('이름')
+$.extend({
+    getUrlVars: function(){
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    },
+    getUrlVar: function(name) {
+        return $.getUrlVars()[name];
+    }
+});
